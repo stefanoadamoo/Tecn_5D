2dbd7a78a80d588070bebca8956a87fbee0e36b0
stefanoadamoo <adamo.stefano@iisgalvanimi.edu.it>
2023-11-23 09:20:55 +0000

    Lavoro COMPLETATO

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const path = require('path')
-
+const path = require('path');
 var express = require('express');
 var app = express();
-app.get ('/', function (req,res) {
+
+app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, 'about.html'))
+});
+
+app.get('/sitemap', function(req, res){
+    res.sendFile(path.join(__dirname, 'sitemap.html'))
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000');
 });
